005ad4cd156acba273ffe4bd636b28c7e2ea6797
GyuYong Jung <obliviscence@gmail.com>
2021-06-24 01:37:53 +0900

    lily58: fix layers

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,17 +4,18 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
-#define L_QWERTY (1 << 4)
-#define L_NAV (1 << 5)
-#define L_MOUSE (1 << 6)
-#define L_MEDIA (1 << 7)
-#define L_NUM (1 << 8)
-#define L_FUN (1 << 9)
-#define L_SYM (1 << 10)
+// #define L_LOWER (1 << 1)
+// #define L_RAISE (1 << 2)
+// #define L_ADJUST (1 << 3)
+// #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_QWERTY (1 << 1)
+// #define L_NAV (1 << 2)
+#define L_MEDIA (1 << 3)
+#define L_NAV (1 << 4)
+#define L_MOUSE (1 << 5)
+#define L_SYM (1 << 6)
+#define L_NUM (1 << 7)
+#define L_FUN (1 << 8)
 
 char layer_state_str[24];
 
@@ -24,16 +25,16 @@ const char *read_layer_state(void) {
   case L_BASE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
+  /* case L_RAISE: */
+  /*   snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise"); */
+  /*   break; */
+  /* case L_LOWER: */
+  /*   snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower"); */
+  /*   break; */
+  /* case L_ADJUST: */
+  /* case L_ADJUST_TRI: */
+  /*   snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust"); */
+  /*   break; */
   case L_QWERTY:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty");
     break;
